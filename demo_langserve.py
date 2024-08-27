0b6cedc2358a8dbacf90642b6ea0e2b031722d2a
Samin-Sadaf7 <95418091+Samin-Sadaf7@users.noreply.github.com>
2024-08-27 15:36:01 +0600

    created the model in langserve

--- demo_langserve.py
+++ demo_langserve.py
@@ -6,9 +6,13 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langserve import add_routes
 
+#Create the prompts
 system_template = "Translate the following into {language}:"
 
 prompt_template = ChatPromptTemplate.from_messages([
     ('system', system_template),
     ('user', '{text}')
 ])
+
+# Create the model
+model = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
