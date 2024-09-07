d839914d1fd9d7f52df5cc148dfacb22a6fc7c4a
Samin-Sadaf7 <95418091+Samin-Sadaf7@users.noreply.github.com>
2024-09-07 16:10:23 +0600

    Set up encoder and decoder

--- my-gpt/bigram.py
+++ my-gpt/bigram.py
@@ -11,3 +11,15 @@ learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 
+torch.manual_seed(1337)
+
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+encode = lambda s : [stoi[c] for c in s]
+decode = lambda l : ''.join([itos[i] for i in l])
