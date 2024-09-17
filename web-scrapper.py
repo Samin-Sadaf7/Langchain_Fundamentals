aee011594511dab5fd808971d4ee759dc749f51f
Samin-Sadaf7 <95418091+Samin-Sadaf7@users.noreply.github.com>
2024-09-17 12:24:36 +0600

    Findall error fixed

--- web-scrapper.py
+++ web-scrapper.py
@@ -1,12 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Fetch the page content
 page_to_scrape = requests.get('https://quotes.toscrape.com/')
-soup = BeautifulSoup(page_to_scrape, "html.parser")
-quotes = soup.findall("span", attrs={'class':'text'})
-autohors = soup.findall("span", attrs={'class':'author'})
+# Parse the content with BeautifulSoup
+soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 
+# Find all the quotes and authors
+quotes = soup.find_all("span", attrs={'class': 'text'})
+authors = soup.find_all("span", attrs={'class': 'author'})
+
+# Print each quote
 for quote in quotes:
     print(quote.text)
-for author in autohors:
+
+# Print each author
+for author in authors:
     print(author.text)
